19b86deb831d50a3368ddb044d0baaa7c5ffa3a7
Fernanda Salas <mfssanhueza@gmail.com>
2019-05-08 12:41:33 -0300

    Ejercicio6

---
+++ ej6.rb
@@ -0,0 +1,22 @@
+# a = 'verdadero'
+# b = 'falso'
+# if a == 'verdadero'
+# puts ':)'
+# else
+# if b == 'verdadero'
+# puts ':|'
+# else
+# puts ':('
+# end
+# end
+
+a = true
+b = true
+
+if a == true && b == true
+    puts ':)'
+elsif a == true || b == true
+    puts ':|'
+else
+    puts ':('
+end
