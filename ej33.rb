ca5b7fbddc6518f0031301bbc7b958e61f59ebf6
Fernanda Salas <mfssanhueza@gmail.com>
2019-05-10 11:46:01 -0300

    Ejercicio 3.3 sin resolver

---
+++ ej33.rb
@@ -0,0 +1,3 @@
+# Construir un programa que permita ingresar un número por teclado e imprimir la tabla de multiplicar
+# del número ingresado.
+# Debe repetir la operación (volver a preguntar por un número) hasta que se ingrese un 0 (cero).
